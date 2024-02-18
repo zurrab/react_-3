f7909b79365115864ef1ce3cbb89ea81ab6f61d2
zurab <126112859+zurrab@users.noreply.github.com>
2024-02-19 02:25:54 +0400

    Add files via upload

---
+++ davaleba_3/src/App.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import NameTable from './NameTable'; 
+
+function App() {
+  return (
+    <div>
+      <h1>Names</h1>
+      <NameTable />
+    </div>
+  );
+}
+
+export default App;
